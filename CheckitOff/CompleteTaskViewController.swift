87edf8007037389cc2378d7676814d569d0c4835
Brian Collings <LimpLion@Brians-MacBook-Pro.local>
2017-05-08 12:51:27 -0400

    app is working, no core data

---
+++ CheckitOff/CompleteTaskViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CompleteTaskViewController.swift
+//  CheckitOff
+//
+//  Created by Brian Collings on 5/7/17.
+//  Copyright © 2017 Morty's Code. All rights reserved.
+//
+
+import UIKit
+
+class CompleteTaskViewController: UIViewController {
+    
+    @IBOutlet weak var taskLabel: UILabel!
+    var task = Task()
+    var previousVC = TasksViewController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if task.important {
+            taskLabel.text = "❗️\(task.name)"
+        } else {
+            taskLabel.text = task.name
+        }
+        
+    }
+    
+    @IBAction func completeTapped(_ sender: Any) {
+        previousVC.tasks.remove(at: previousVC.selectedIndex)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
+    }
+    
+    
+    
+    
+    
+}
---
+++ CheckitOff/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  CheckitOff
+//
+//  Created by Brian Collings on 4/27/17.
+//  Copyright © 2017 Morty's Code. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
